3c00bf883c15c262da5bc604a2d9223b6d3f9edc
Khulood Alhamed <Khuloodalhamed@gmail.com>
2022-10-30 09:19:38 +0300

    Create EventPage

---
+++ Rankit/EventPage.swift
@@ -0,0 +1,20 @@
+//
+//  EventPage.swift
+//  Rankit
+//
+//  Created by Khulood Alhamed on 30/10/2022.
+//
+
+import SwiftUI
+
+struct EventPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventPage_Previews: PreviewProvider {
+    static var previews: some View {
+        EventPage()
+    }
+}
