b68a1e5947d88ea511a012658c822f8916ef4791
RandAlMuhanna <Rand.y.almuhanna@gmail.com>
2022-10-31 21:56:15 +0300

    ربط للصفحات وتعديلات على بعض الواجهات

---
+++ Rankit/EventsPage.swift
@@ -0,0 +1,20 @@
+//
+//  EventsPage.swift
+//  Rankit
+//
+//  Created by Rand AlMuhanna on 06/04/1444 AH.
+//
+
+import SwiftUI
+
+struct EventsPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventsPage_Previews: PreviewProvider {
+    static var previews: some View {
+        EventsPage()
+    }
+}
