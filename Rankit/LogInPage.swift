4881ec4951598801f0f127e41d4174ee2f05586c
Wala <wialmuheef22@twq.idserve.net>
2022-10-31 23:30:14 +0300

    log in with apple page

--- Rankit/LogInPage.swift
+++ Rankit/LogInPage.swift
@@ -6,15 +6,70 @@
 //
 
 import SwiftUI
+import AuthenticationServices
+
 
 struct LogInPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView{
+            
+            
+                VStack(){
+                        Image("coloredStar")
+                            .resizable()
+                            .frame(width: 130.0, height: 130.0)
+                            .padding()
+                        
+                        Text("Rank your favorite events, and inspire others to explore the joy")
+                            .font(.system(size: 16, weight: .bold))
+                            .multilineTextAlignment(.center)
+                            .padding(.bottom, 50)
+                        
+                        
+                        ZStack(){
+                            Button("Sign in with Apple") {}
+                                .frame(maxWidth: .infinity, maxHeight: 57)
+                                .foregroundColor(.white)
+                                .background(Color.black)
+                                .cornerRadius(8)
+                            
+                            Image(systemName:"envelope.circle.fill")
+                                .foregroundColor(.red)
+                            
+                        }.padding(.bottom, 17)
+                        
+                        ZStack(){
+                            
+                            Button("Sign up with email") {}
+                                .frame(maxWidth: .infinity , maxHeight: 57)
+                                .foregroundColor(.white)
+                                .background(Color(red: 94/255, green: 126/255, blue: 152/255))
+                                .cornerRadius(8)
+                            
+                            Image(systemName:"envelope.circle.fill")
+                                .foregroundColor(.red)
+                        }.padding(.bottom, 70)
+                        
+                    
+                    HStack{
+                        Text("Already Have an  Account?")
+                        Button("Log in"){}
+                            .foregroundColor(Color(red: 94/255, green: 126/255, blue: 152/255))
+                    }
+                    
+                    
+                }.padding()
+                
+                
+            }
+        }
     }
-}
+
 
 struct LogInPage_Previews: PreviewProvider {
     static var previews: some View {
         LogInPage()
     }
 }
+
