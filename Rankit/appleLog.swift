5e6642e41e8ef07882a754b139424201ef7db5cc
Wala <wialmuheef22@twq.idserve.net>
2022-11-01 12:45:31 +0300

    last update

--- Rankit/appleLog.swift
+++ Rankit/appleLog.swift
@@ -22,25 +22,28 @@ struct appleLog: View {
                             .font(.system(size: 16, weight: .bold))
                             .multilineTextAlignment(.center)
                             .padding(.bottom, 50)
+                    
                         
                         
                         ZStack(){
                             Button("Sign in with Apple") {}
+                                .padding(.leading, 30.0)
                                 .frame(maxWidth: .infinity, maxHeight: 57)
                                 .foregroundColor(.white)
                                 .background(Color.black)
                                 .cornerRadius(8)
                             
-                            Image(systemName:"envelope.circle.fill")
+                            Image(systemName:"apple.logo")
                                 .foregroundColor(.white)
                                 .font(.system(size: 25))
-                                .padding(.trailing, 200)
+                                .padding(.trailing, 170)
                             
                         }.padding(.bottom, 17)
                         
                         ZStack(){
                             
                             Button("Sign up with email") {}
+                                .padding(.leading, 25.0)
                                 .frame(maxWidth: .infinity , maxHeight: 57)
                                 .foregroundColor(.white)
                                 .background(Color(red: 94/255, green: 126/255, blue: 152/255))
@@ -49,7 +52,7 @@ struct appleLog: View {
                             Image(systemName:"envelope.circle.fill")
                                 .foregroundColor(.white)
                                 .font(.system(size: 25))
-                                .padding(.trailing, 200)
+                                .padding(.trailing, 170)
                             
                         }.padding(.bottom, 70)
                         
